4f515fa6891dcbce32249c01e0a5287a84bf2b2e
danfsaputra <mochdaniferdiansaputra@gmail.com>
2025-07-22 09:28:31 +0700

    og berita

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -181,19 +181,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("749674b3cc9be1f0")
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
+/******/ 		__webpack_require__.h = () => ("1d6c5a2694e34293")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/webpack/webpack.4edc11110b1cdf26.hot-update.js
+++ .next/static/webpack/webpack.4edc11110b1cdf26.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("50a9d797a6c292ca")
+/******/ 	__webpack_require__.h = () => ("1d6c5a2694e34293")
 /******/ })();
 /******/ 
 /******/ }
